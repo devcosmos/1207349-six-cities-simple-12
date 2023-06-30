25544e40fc5e9bfe9c80dce43544513538501fc0
cosmos <va-litv@yandex.ru>
2023-06-30 14:52:58 +0300

    add basename to room host

--- project/src/components/room-host/room-host.tsx
+++ project/src/components/room-host/room-host.tsx
@@ -1,3 +1,4 @@
+import { BASENAME } from '../../const';
 import { Host } from '../../types/offer';
 
 type RoomHostProps = {
@@ -13,7 +14,7 @@ function RoomHost({host, description}: RoomHostProps): JSX.Element {
       <h2 className="property__host-title">Meet the host</h2>
       <div className="property__host-user user">
         <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-          <img className="property__avatar user__avatar" src={avatarUrl} width="74" height="74" alt={name} />
+          <img className="property__avatar user__avatar" src={`${BASENAME}/${avatarUrl}`} width="74" height="74" alt={name} />
         </div>
         <span className="property__user-name">{name}</span>
         {isPro && <span className="property__user-status">Pro</span>}
